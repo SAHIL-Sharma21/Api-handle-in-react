f46f6b2f116d407cf831eaaef479a5f5d698aa9f
Sahil Sharma <sahilsharma2000dtc@gmail.com>
2024-02-06 22:11:27 +0530

    completed edge cases

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,28 +6,36 @@ function App() {
   
   const [products, setProducts] = useState([]);
   const [error, setError] = useState(false); //best practice is take error in state and we can talk this about in interview -- one of the edge cases
+  const [loading, setLoading] = useState(false); // edge case when request is made and we are showing user loading that data is loading and it wil take time.
 
   useEffect(() => {
     //if we dont have to use .then method then we will do it by using IIFE
 
     try {
+      setLoading(true);
       setError(false); //if we again fetch the data then we have to set error false.
       ;(async() => { // just for saftey ppurpose we use semicolon (;) as IIFE is tricky to use it means all the previous work is done and new IIFE start
       const response =  await axios.get('/api/products');
-      // console.log(response);
-      setProducts(response.data)
+      console.log(response.data);
+      setProducts(response.data);
+      setLoading(false);
       })(); //IIFE
     } catch (error) {
       setError(true);
+      setLoading(false);
     }
 
   }, []);
 
 //handling error
 if(error) {
-  <h1>Something went wrong!!</h1>
+ return <h1>Something went wrong!!</h1>
 }
-  
+
+// handlling loading
+if(loading) {
+  return <h1>Loading....</h1>
+}
 
   //defining handledata functionality
   return (
