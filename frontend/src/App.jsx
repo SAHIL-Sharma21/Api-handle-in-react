2dea240a79ff2d66796c4e2efad30e8f8c410a18
Sahil Sharma <sahilsharma2000dtc@gmail.com>
2024-02-06 21:35:03 +0530

    axios added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,29 @@
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css'
 
 function App() {
   
+  const [products, setProducts] = useState([]);
 
+  useEffect(() => {
+    //if we dont have to use .then method then we will do it by using IIFE
+
+    (async() => {
+    const response =  await axios.get('/api/products');
+    // console.log(response);
+    setProducts(response.data)
+    })(); //IIFE
+
+  }, []);
+
+  //defining handledata functionality
   return (
     <>
       <h1>Handling api in react </h1>
+      <h2>total number of products: {products.length}</h2>
+
+      {/* <button onClick={handledata}>Get data </button> */}
     </>
   )
 }
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  //setting proxy fro not getting CORS Error
+  server: {
+    proxy: {
+      '/api': "http://localhost:3000",
+    }
+  },
   plugins: [react()],
 })
