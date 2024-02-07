8931e5a4a32e4b27d5aa7cc7a7ed18e2bd87d253
Sahil Sharma <sahilsharma2000dtc@gmail.com>
2024-02-07 22:32:54 +0530

    customReactQuery

---
+++ frontend/src/customReactQuery.js
@@ -0,0 +1,30 @@
+//here we are making custom react query method
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const customReactQuery = (path) => {
+
+    const [products, setProducts] = useState([]);
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        ; (async () => {
+            try {
+                setLoading(true);
+                setError(false);
+                const response = await axios.get(path);
+                setProducts(response.data);
+                setLoading(false);
+
+            } catch (error) {
+                setError(true);
+                setLoading(false);
+            }
+        })();
+
+    }, []);
+    return { products, error, loading }
+}
+
+export default customReactQuery;
